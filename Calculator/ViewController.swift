e8c6a424b21c773dcbc171a457f323cb2a2df4dd
felipe-zg <felipe_zeba@outlook.com>
2021-11-06 21:02:17 -0500

    feat: add basic calculator functions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,21 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     @IBOutlet weak var displayLabel: UILabel!
-    
+    private var isFinishTypingNumber = true
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
+       isFinishTypingNumber = true
         
-        //What should happen when a non-number button is pressed
-    
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert current display value String to Bouble")
+        }
+        
+        if let calcMethod = sender.currentTitle {
+            switch calcMethod {
+            case "AC":
+                displayLabel.text = "0"
+            case "+/-":
+                displayLabel.text = String(number * -1)
+            case "%":
+                displayLabel.text = String(number / 100)
+            default:
+                print("default")
+            }
+        }
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle {
+            if isFinishTypingNumber {
+                displayLabel.text = numValue
+                isFinishTypingNumber = false
+            } else {
+                if numValue == "." && displayLabel.text!.contains("."){
+                    return
+                }
+                displayLabel.text = displayLabel.text?.appending(numValue)
+            }
+        }
     
     }
 
